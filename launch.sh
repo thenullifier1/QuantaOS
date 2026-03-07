9e4af07307bdc1bebafd7b5b87023a5477a77a69
Your Name <your@email.com>
2026-03-07 14:19:34 +0300

    Auto-clone + launch in one command

--- launch.sh
+++ launch.sh
@@ -1,6 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/bash
-pkg install tmux -y 2>/dev/null
+pkg install -y tmux nodejs git openssh 2>/dev/null
+[ ! -d ~/QuantaOS ] && git clone https://github.com/thenullifier1/QuantaOS ~/QuantaOS && cd ~/QuantaOS && npm install
 kill $(lsof -t -i:3000) 2>/dev/null
-tmux new-session -d -s quantaos 2>/dev/null || tmux kill-session -t quantaos && tmux new-session -d -s quantaos
+tmux kill-session -t quantaos 2>/dev/null
+tmux new-session -d -s quantaos
 tmux send-keys -t quantaos "cd ~/QuantaOS && node server.js & ssh -R 80:localhost:3000 ssh.localhost.run" Enter
 tmux attach -t quantaos
